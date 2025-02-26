9787d8bb9569ee037e40cdf317fd4254ae7bf4bc
byhaqii <rizalbaihaqiiiiii@gmail.com>
2025-02-26 20:22:28 +0700

    Praktikum 5

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LevelController;
+use App\Http\Controllers\KategoriController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::get('/level', [LevelController::class,'index']);
+Route::get('/kategori', [KategoriController::class, 'index']);
 

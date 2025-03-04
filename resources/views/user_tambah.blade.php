9177449ee6c3df576b8026d21ac48e0eafe3ea6c
byhaqii <rizalbaihaqiiiiii@gmail.com>
2025-03-04 13:01:23 +0700

    Praktikum 2.6 – Create, Read, Update, Delete (CRUD)

---
+++ resources/views/user_tambah.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+</head>
+<body>
+    <h1>Form Tambah Data User</h1>
+    <form method="post" action="/user/tambah_simpan">
+
+        {{ csrf_field() }}
+
+        <label>Username</label>
+        <input type="text" name="username" placeholder="Masukan Username">
+        <br>
+
+        <label>Name</label>
+        <input type="text" name="name" placeholder="Masukan Nama">
+        <br>
+
+        <label>Password</label>
+        <input type="password" name="password" placeholder="Masukan Password">
+        <br>
+
+        <label>Level ID</label>
+        <input type="number" name="level_id" placeholder="Masukan ID Level">
+        <br><br>
+
+        <input type="submit" class="btn btn-success" value="Simpan">
+    </form>
+</body>
+
+</html>
